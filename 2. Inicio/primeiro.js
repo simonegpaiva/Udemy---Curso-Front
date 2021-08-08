2b1081598a889503dc07883eb19cdbea8d812b05
Simone Paiva <73961122+simonegpaiva@users.noreply.github.com>
2021-08-07 21:25:45 -0300

    first

---
+++ 2. Inicio/primeiro.js
@@ -0,0 +1,8 @@
+
+//Sentença de código - Uma linha de codigo completa
+console.log('Olá')
+console.log('Boa Noite')
+console.log('Cansada')
+
+//Bloco de código
+
---
+++ 2. Inicio/sentenca.js
@@ -0,0 +1,15 @@
+//Comentarios dentro do código use barra-barra //
+console.log('Olá Mundo')
+console.log('2 + 3')
+console.log(2+3)
+
+//No Javascript pode usar o ponto e virgula ou não, não é obrigatorio
+//Assim, como pode-se usar aspas simples ou aspas duplas
+
+/*Para comentar mais de uma linha usamos
+este comando barra, asterisco - texto -  asterico, barra*/
+
+//Blocos de Código
+{
+    console.log('Caramba')
+}
